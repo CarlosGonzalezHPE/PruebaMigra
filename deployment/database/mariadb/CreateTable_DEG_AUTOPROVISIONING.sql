d62dc871ac2a415bbba30a21dfc9a4879a5cdf9f
rmnavarro <rmnavarro@1b308f40-575c-0510-bb77-a5e8b53bb2f9>
2018-02-07 10:53:57 +0000

    Cambios Autoprovisión & Blacklist de rangos de IMSIs
    git-svn-id: https://draco.esm.cpqcorp.net/svn/DEG_ORANGE/branches/delivery@46 1b308f40-575c-0510-bb77-a5e8b53bb2f9

---
+++ deployment/database/mariadb/CreateTable_DEG_AUTOPROVISIONING.sql
@@ -0,0 +1,23 @@
+
+-- Delivery datamodel changes for DEG Orange
+-- HPE 2018
+
+-- AUTOPROVISIONING TABLE:
+
+DROP TABLE IF EXISTS DEG_AUTOPROVISIONING;
+
+CREATE TABLE DEG_AUTOPROVISIONING(
+        TIMESTAMP_QUERY               DATETIME,
+        TIMESTAMP_SYSTEM              DATETIME,
+        USERNAME                      VARCHAR(64) NOT NULL,
+        NODE_ID                       VARCHAR(64),
+        COMMAND                       VARCHAR(64),
+        UNIQUE_ID                     VARCHAR(64),
+        REALM                         VARCHAR(64),
+        DETAILS                       VARCHAR(64),
+        REQUEST_ID                    VARCHAR(64),
+        RESULT_CODE                   VARCHAR(64),
+        PROVISIONED                   VARCHAR(64) NOT NULL);
+
+ALTER TABLE DEG_AUTOPROVISIONING ADD CONSTRAINT PK_DEG_AUTOPROVISIONING PRIMARY KEY(USERNAME, PROVISIONED);
+CREATE INDEX DEG_AUTOPROVISIONING_IDX ON DEG_AUTOPROVISIONING(USERNAME, PROVISIONED);
