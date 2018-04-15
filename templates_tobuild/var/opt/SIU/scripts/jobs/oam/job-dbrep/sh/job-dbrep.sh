0ba357ed9223e345df0c5ab8e2c8dd6c4a0cbd05
chcremy <chcremy@1b308f40-575c-0510-bb77-a5e8b53bb2f9>
2018-04-15 17:24:32 +0000

    Implementando script de reparo de la replicacion. No completado
    git-svn-id: https://draco.esm.cpqcorp.net/svn/DEG_ORANGE/branches/delivery@158 1b308f40-575c-0510-bb77-a5e8b53bb2f9

--- templates_tobuild/var/opt/SIU/scripts/jobs/oam/job-dbrep/sh/job-dbrep.sh
+++ templates_tobuild/var/opt/SIU/scripts/jobs/oam/job-dbrep/sh/job-dbrep.sh
@@ -53,12 +53,12 @@ function process
   logDebug "Executing function 'process'"
 
   OUTPUT_DIR=${WORK_DIR}/$(date +"%Y%m%d%H%M")
-  mkdir -p ${OUTPUT_DIR}
+  mkdir -p ${OUTPUT_DIR}/remote
 
-  >/dev/null 2>&1 cd ${OUTPUT_DIR}
+  >/dev/null 2>&1 cd ${OUTPUT_DIR}/remote
   if [ $? -ne 0 ]
   then
-    logError "Unable to access to directory '${OUTPUT_DIR}'"
+    logError "Unable to access to directory '${OUTPUT_DIR}/remote'"
     >/dev/null 2>&1 cd -
     return 1
   fi
@@ -88,6 +88,14 @@ function process
   fi
   logDebug "IGNORED_LAST_MINUTES = ${IGNORED_LAST_MINUTES}"
 
+  SLEEP_MINUTES_BEFORE_FECTH="$(getConfigParam GENERAL SLEEP_MINUTES_BEFORE_FECTH)"
+  if [ $? -lt 0 ] || [ -z ${SLEEP_MINUTES_BEFORE_FECTH} ]
+  then
+    logError "Unable to get mandatory parameter 'SLEEP_MINUTES_BEFORE_FECTH' in section 'GENERAL'"
+    return 1
+  fi
+  logDebug "SLEEP_MINUTES_BEFORE_FECTH = ${SLEEP_MINUTES_BEFORE_FECTH}"
+
   REMOTE_PEER_IPADDRESS="$(getConfigParam GENERAL REMOTE_PEER_IPADDRESS)"
   if [ $? -lt 0 ] || [ -z ${REMOTE_PEER_IPADDRESS} ]
   then
@@ -97,7 +105,8 @@ function process
   logDebug "REMOTE_PEER_IPADDRESS = ${REMOTE_PEER_IPADDRESS}"
 
   TIMESTAMP_NOW=$(date +"%s")
-  TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES=$(date +"%s" -d "${IGNORED_LAST_MINUTES} minutes ago")
+  TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES=$(date +"%s" -d "-${IGNORED_LAST_MINUTES} minutes")
+  TIMESTAMP_START_FETCHING_REMOTE_FILES=$(date +"%s" -d "+${SLEEP_MINUTES_BEFORE_FECTH} minutes")
   logDebug "TIMESTAMP_NOW = ${TIMESTAMP_NOW}"
   logDebug "TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES = ${TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES}"
 
@@ -113,17 +122,17 @@ function process
     return 0
   fi
 
-  RETURN_CODE=0
+  logDebug "Dumping local tables"
 
   while read TABLE
   do
-    logDebug "Processing table '${TABLE}'"
+    logInfo "Dumping table '${TABLE}'"
 
     DB_INSTANCE="$(getConfigParam ${TABLE} DB_INSTANCE)"
     if [ $? -lt 0 ] || [ -z ${DB_INSTANCE} ]
     then
       logError "Unable to get mandatory parameter 'DB_INSTANCE' in section '${TABLE}'"
-      RETURN_CODE=1
+      touch ${TMP_DIR}/${TABLE}.failed
       continue
     fi
     logDebug "DB_INSTANCE = ${DB_INSTANCE}"
@@ -132,7 +141,7 @@ function process
     if [ $? -lt 0 ] || [ -z ${DB_INSTANCE} ]
     then
       logError "Unable to get mandatory parameter 'DB_DSN' in section '${TABLE}'"
-      RETURN_CODE=1
+      touch ${TMP_DIR}/${TABLE}.failed
       continue
     fi
     logDebug "DB_DSN = ${DB_DSN}"
@@ -141,7 +150,7 @@ function process
     if [ $? -lt 0 ] || [ -z "${DATETIME_FIELD}" ]
     then
       logError "Unable to get mandatory parameter 'DATETIME_FIELD' in section '${TABLE}'"
-      RETURN_CODE=1
+      touch ${TMP_DIR}/${TABLE}.failed
       continue
     fi
     logDebug "DATETIME_FIELD = ${DATETIME_FIELD}"
@@ -150,7 +159,8 @@ function process
     if [ $? -lt 0 ] || [ -z "${DATETIME_FORMAT}" ]
     then
       logError "Unable to get mandatory parameter 'DATETIME_FORMAT' in section '${TABLE}'"
-      RETURN_CODE=1
+      touch ${TMP_DIR}/${TABLE}.failed
+      > ${TMP_DIR}/${TABLE}.failed
       continue
     fi
     logDebug "DATETIME_FORMAT = ${DATETIME_FORMAT}"
@@ -159,27 +169,27 @@ function process
     if [ $? -lt 0 ]
     then
       logError "Unable to get mandatory section '${TABLE}.FIELDS'"
-      RETURN_CODE=1
+      touch ${TMP_DIR}/${TABLE}.failed
       continue
     fi
 
     if [ ! -s ${TMP_DIR}/${TABLE}.fields ]
     then
       logError "No fields defined for table '${TABLE}'"
-      RETURN_CODE=1
+      touch ${TMP_DIR}/${TABLE}.failed
       continue
     fi
 
     if [ ! -s ${CTRL_DIR}/${TABLE}.timestamp_last_execution ]
     then
-      TIMESTAMP_LAST_EXECUTION=$(date +"%s" -d "${DEFAULT_INTERVAL_MINUTES} minutes ago")
+      TIMESTAMP_LAST_EXECUTION=$(date +"%s" -d "-${DEFAULT_INTERVAL_MINUTES} minutes")
     else
       TIMESTAMP_LAST_EXECUTION=$(cat ${CTRL_DIR}/${TABLE}.timestamp_last_execution | head -n 1 | grep -P "^\d{10}$")
 
-      if [ -z ${TIMESTAMP_LAST_EXECUTION} ] || [ ${TIMESTAMP_LAST_EXECUTION} -lt $(date +"%s" -d "${MAX_INTERVAL_MINUTES} minutes ago") ] || [ ${TIMESTAMP_LAST_EXECUTION} -ge $(date +"%s") ]
+      if [ -z ${TIMESTAMP_LAST_EXECUTION} ] || [ ${TIMESTAMP_LAST_EXECUTION} -lt $(date +"%s" -d "-${MAX_INTERVAL_MINUTES} minutes") ] || [ ${TIMESTAMP_LAST_EXECUTION} -ge $(date +"%s") ]
       then
         logWarning "Invalid value '${TIMESTAMP_LAST_EXECUTION}' for parameter 'TIMESTAMP_LAST_EXECUTION'. Set to default"
-        TIMESTAMP_LAST_EXECUTION=$(date +"%s" -d '${DEFAULT_INTERVAL_MINUTES} minutes ago')
+        TIMESTAMP_LAST_EXECUTION=$(date +"%s" -d '-${DEFAULT_INTERVAL_MINUTES} minutes')
       fi
     fi
     logDebug "TIMESTAMP_LAST_EXECUTION = ${TIMESTAMP_LAST_EXECUTION}"
@@ -187,34 +197,68 @@ function process
     if [ ${TIMESTAMP_LAST_EXECUTION} -ge ${TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES} ]
     then
       logWarning "Ignored last minutes '${IGNORED_LAST_MINUTES}' have not been reached yet. Table is skipped"
+      touch ${TMP_DIR}/${TABLE}.skipped
       continue
     fi
 
     case "${DATETIME_FORMAT}" in
       "DATETIME")
+        SELECT_DATETIME="SELECT UNIX_TIMESTAMP(${DATETIME_FIELD})"
         DATETIME_CONDITION="UNIX_TIMESTAMP(${DATETIME_FIELD}) >= ${TIMESTAMP_LAST_EXECUTION} AND UNIX_TIMESTAMP(${DATETIME_FIELD}) < ${TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES}"
         ;;
       "TIMESTAMP")
+        SELECT_DATETIME="SELECT ${DATETIME_FIELD} DIV 1000"
         DATETIME_CONDITION="${DATETIME_FIELD} >= ${TIMESTAMP_LAST_EXECUTION}000 AND ${DATETIME_FIELD} < ${TIMESTAMP_BEFORE_IGNORED_LAST_MINUTES}000"
         ;;
       *)
         logError "Unsupported datetime format '${DATETIME_FORMAT}'"
-        RETURN_CODE=1
+        touch ${TMP_DIR}/${TABLE}.failed
         continue
         ;;
     esac
 
-    AUX_SQL_QUERY="SELECT "
-    while read FIELD
+    AUX_SELECT_KEY=
+    AUX_SELECT_FIELDS=
+    while read FIELD_LINE
     do
-      AUX_SQL_QUERY=${AUX_SQL_QUERY}${FIELD}","
+      FIELD_NAME=$(echo ${FIELD_LINE} | cut -d ";" -f 1)
+      FIELD_TYPE=$(echo ${FIELD_LINE} | cut -d ";" -f 2)
+      FIELD_ATTRIBUTE=$(echo ${FIELD_LINE} | cut -d ";" -f 3)
+
+      if [ "${FIELD_ATTRIBUTE}" = "key" ]
+      then
+        AUX_SELECT_KEY=${AUX_SELECT_KEY}"'"${FIELD_NAME}"=',"${FIELD_NAME}",';',"
+      else
+        AUX_SELECT_FIELDS=${AUX_SELECT_FIELDS}${FIELD_NAME}","
+      fi
     done < ${TMP_DIR}/${TABLE}.fields
 
-    SQL_QUERY=$(echo ${AUX_SQL_QUERY} | sed 's/.$//')" FROM ${TABLE} WHERE ${DATETIME_CONDITION} ORDER BY ${DATETIME_FIELD}"
+    logDebug "AUX_SELECT_KEY = ${AUX_SELECT_KEY}"
+    logDebug "AUX_SELECT_FIELDS = ${AUX_SELECT_FIELDS}"
+
+    SELECT_KEY=${AUX_SELECT_KEY/%?????/}
+    if [ -z ${SELECT_KEY} ]
+    then
+      logError "No key defined to select data from table '${TABLE}'"
+      touch ${TMP_DIR}/${TABLE}.failed
+      continue
+    fi
+    logDebug "SELECT_KEY = ${SELECT_KEY}"
+
+    SELECT_FIELDS=${AUX_SELECT_FIELDS/%?/}
+    if [ -z ${SELECT_FIELDS} ]
+    then
+      logError "No fields defined to select data from table '${TABLE}'"
+      touch ${TMP_DIR}/${TABLE}.failed
+      continue
+    fi
+    logDebug "SELECT_FIELDS = ${SELECT_FIELDS}"
+
+    SQL_QUERY="CONCAT_WS('', ${SELECT_KEY}), ${SELECT_DATETIME}, ${SELECT_FIELDS} FROM ${TABLE} WHERE ${DATETIME_CONDITION} ORDER BY ${DATETIME_FIELD}"
     logDebug "SQL_QUERY = ${SQL_QUERY}"
 
     echo ${SQL_QUERY} > ${TMP_DIR}/query-${TABLE}.sql
-    echo "INTO OUTFILE '"${OUTPUT_DIR}/query-${TABLE}.csv"'" >> ${TMP_DIR}/query-${TABLE}.sql
+    echo "INTO OUTFILE '"${OUTPUT_DIR}/dump-${TABLE}.csv"'" >> ${TMP_DIR}/query-${TABLE}.sql
     echo "FIELDS TERMINATED BY ','" >> ${TMP_DIR}/query-${TABLE}.sql
     echo "LINES TERMINATED BY '\n';" >> ${TMP_DIR}/query-${TABLE}.sql
 
@@ -222,16 +266,244 @@ function process
 
     if [ $(grep -i error ${TMP_DIR}/query-${TABLE}.out | wc -l) -gt 0 ] || [ -s ${TMP_DIR}/query-${TABLE}.err ]
     then
-      logError "Command '> ${TMP_DIR}/query-${TABLE}.out 2> ${TMP_DIR}/query-${TABLE}.err /usr/bin/mysql -sN -S /var/Mariadb/${DB_INSTANCE}/mysql.sock -u root -D ${DB_DSN} -e "source ${TMP_DIR}/query-${TABLE}.sql"' failed"
-      RETURN_CODE=1
+      logError "Command '> ${TMP_DIR}/query-${TABLE}.out 2> ${TMP_DIR}/query-${TABLE}.err /usr/bin/mysql -sN -S /var/Mariadb/${DB_INSTANCE}/mysql.sock -u root -D ${DB_DSN} -e \"source ${TMP_DIR}/query-${TABLE}.sql\"' failed"
+      touch ${TMP_DIR}/${TABLE}.failed
       continue
     fi
+
+    if [ ! -s ${OUTPUT_DIR}/dump-${TABLE}.csv ]
+    then
+      touch ${TMP_DIR}/${TABLE}.skipped
+      continue
+    fi
+  done < ${TMP_DIR}/tables
+
+  let SLEEP_SECONDS=${TIMESTAMP_START_FETCHING_REMOTE_FILES}-$(date +"%s")
+
+  if [ ${SLEEP_SECONDS} -gt 0 ]
+  then
+    logInfo "Sleeping for ${SLEEP_SECONDS} seconds"
+    sleep ${SLEEP_SECONDS}
+  fi
+
+  logInfo "Fetching dump of remote tables"
+
+  while read TABLE
+  do
+    logInfo "Fetching dump of remote table '${TABLE}'"
+
+    > ${TMP_DIR}/scp-${TABLE}.out 2> ${TMP_DIR}/scp-${TABLE}.err scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ium@${REMOTE_PEER_IPADDRESS}:${OUTPUT_DIR}/dump-${TABLE}.csv ${OUTPUT_DIR}/remote
+    if [ $? -ne 0 ]
+    then
+      logError "Command '> ${TMP_DIR}/scp-${TABLE}.out 2> ${TMP_DIR}/scp-${TABLE}.err scp ium@${REMOTE_PEER_IPADDRESS}:${OUTPUT_DIR}/dump-${TABLE}.csv ${OUTPUT_DIR}/remote' failed"
+      touch ${TMP_DIR}/${TABLE}.failed
+      continue
+    fi
+  done < ${TMP_DIR}/tables
+
+  logInfo "Resolving conflicts"
+
+  while read TABLE
+  do
+    CONFLICT_RESOLUTION_ENABLED="$(getConfigParam ${TABLE} CONFLICT_RESOLUTION_ENABLED)"
+    if [ $? -lt 0 ] || [ -z "${CONFLICT_RESOLUTION_ENABLED}" ]
+    then
+      logWarning "Parameter 'CONFLICT_RESOLUTION_ENABLED' not defined in section '${TABLE}'. Set to default value 'TRUE"
+      CONFLICT_RESOLUTION_ENABLED=TRUE
+    else
+      case "${CONFLICT_RESOLUTION_ENABLED}" in
+        "TRUE" | "FALSE")
+          ;;
+        *)
+          CONFLICT_RESOLUTION_ENABLED=TRUE
+          logWarning "Invalid value '${CONFLICT_RESOLUTION_ENABLED}' for parameter  'CONFLICT_RESOLUTION_ENABLED' in section '${TABLE}'. Set to default value 'TRUE"
+          ;;
+      esac
+    fi
+    logDebug "CONFLICT_RESOLUTION_ENABLED = ${CONFLICT_RESOLUTION_ENABLED}"
+
+    if [ "${CONFLICT_RESOLUTION_ENABLED}" = "FALSE" ]
+    then
+      logWarning "Conflict resolution is not enabled for table '${TABLE}'"
+      touch ${TMP_DIR}/${TABLE}.conflict_resolution_disabled
+      continue
+    fi
+
+    logInfo "Computing differences for table '${TABLE}'"
+
+    logDebug "Sorting files"
+
+    cat ${OUTPUT_DIR}/dump-${TABLE}.csv | sort > ${OUTPUT_DIR}/dump-${TABLE}.csv.sorted
+    if [ $? -ne 0 ]
+    then
+      logError "Command 'cat ${OUTPUT_DIR}/dump-${TABLE}.csv | sort > ${OUTPUT_DIR}/dump-${TABLE}.csv.sorted' failed"
+      touch ${TMP_DIR}/${TABLE}.failed
+      continue
+    fi
+
+    cat ${OUTPUT_DIR}/remote/dump-${TABLE}.csv | sort > ${OUTPUT_DIR}/remote/dump-${TABLE}.csv.sorted
+    if [ $? -ne 0 ]
+    then
+      logError "Command 'cat ${OUTPUT_DIR}/remote/dump-${TABLE}.csv | sort > ${OUTPUT_DIR}/remote/dump-${TABLE}.csv.sorted' failed"
+      touch ${TMP_DIR}/${TABLE}.failed
+      continue
+    fi
+
+    diff -W 23 -y ${OUTPUT_DIR}/dump-${TABLE}.csv.sorted ${OUTPUT_DIR}/remote/dump-${TABLE}.csv.sorted > ${TMP_DIR}/dump-${TABLE}.diffs
+    if [ $? -ne 0 ]
+    then
+      logError "Command 'diff -W 23 -y ${OUTPUT_DIR}/dump-${TABLE}.csv.sorted ${OUTPUT_DIR}/remote/dump-${TABLE}.csv.sorted > ${TMP_DIR}/dump-${TABLE}.diffs' failed"
+      touch ${TMP_DIR}/${TABLE}.failed
+      continue
+    fi
+
+    logDebug "Processing diff file"
+
+    awk -F \% -v OUT=${TMP_DIR}/lines.${TABLE}.out -v ERR=${TMP_DIR}/lines.${TABLE}.err '
+    function ltrim(s) {
+      sub(/^[ \t\r\n]+/, "", s);
+      return s;
+    }
+    function rtrim(s) {
+      sub(/[ \t\r\n]+\$/, \"\", s);
+      return s;
+    }
+    function trim(s) {
+      return rtrim(ltrim(s));
+    }
+    BEGIN { line = 0; num_conflicts = 0 } {
+      if ($0  ~ " | ") {
+        split($0, v, "|");
+        local_timestamp = 0 + trim(v[1]);
+        remote_timestamp = 0 + trim(v[2]);
+
+        if (length(local_timestamp) != 10) {
+          print line":"$0 >> ERR;
+          line++;
+          next;
+        }
+
+        if (! match(local_timestamp, /^[0-9]+$/)) {
+          print line":"$0 >> ERR;
+          line++;
+          next;
+        }
+
+        if (length(remote_timestamp) != 10) {
+          print line":"$0 >> ERR;
+          line++;
+          next;
+        }
+
+        if (! match(remote_timestamp, /^[0-9]+$/)) {
+          print line":"$0 >> ERR;
+          line++;
+          next;
+        }
+
+        if (local_timestamp >= remote_timestamp) {
+          num_conflicts++;
+          print line",UPDATE" >> OUT;
+          line++;
+          next;
+        }
+      } else if ($0  ~ " < ") {
+        num_conflicts++;
+        print line",INSERT" >> OUT;
+      } else if ($0  ~ " > ") {
+        num_conflicts++;
+      }
+
+      line++;
+    }
+    END { print num_conflicts } ' ${TMP_DIR}/dump-${TABLE}.diffs > ${TMP_DIR}/conflicts.${TABLE}
+
+    if [ -s ${TMP_DIR}/lines.${TABLE}.err ]
+    then
+      logWarning "Some conflicts won't be resolved"
+      touch ${TMP_DIR}/${TABLE}.warning
+    fi
+
+    let NUM_CONFLICTS=$(cat ${TMP_DIR}/conflicts.${TABLE} | head -n 1)
+    logDebug "NUM_CONFLICTS = NUM_CONFLICTS"
+
+    if [ ${NUM_CONFLICTS} -gt 0 ]
+    then
+      logWarning "Conflicts have been detected in table ${TABLE}"
+    fi
+
+    awk -F \, '
+    BEGIN { index_file = 0; }
+    {
+      if (FNR == 1) {
+        index_file++;
+        line = 0;
+      }
+
+      if (index_file == 1) {
+        lines[$1];
+        operation[$1] = $2;
+        next;
+      }
+
+      if (index_file == 2) {
+        if (line in lines) {
+          print operation[line]";"$0;
+        }
+        line++;
+      }
+    }' ${TMP_DIR}/lines.${TABLE}.out ${OUTPUT_DIR}/dump-${TABLE}.csv.sorted > ${TMP_DIR}/changes.${TABLE}.out
+
+    if [ ! -s ${TMP_DIR}/changes.${TABLE}.out ]
+    then
+      logInfo "No local operations needed"
+      touch ${TMP_DIR}/${TABLE}.skipped
+      continue
+    fi
+
+    awk -F \; -v datetime_field=${DATETIME_FIELD} -v datetime_format=${DATETIME_FORMAT} -v table=${TABLE} '{
+      operation = $1:
+      key_data = $2;
+
+      where = "";
+      split(key_data, v, ",");
+      datetime = v[1];
+      split(v[2], key_avps, ";");
+      for (key_avp in key_avps) {
+        split(key_avp, key, "=");
+        key_field = key[1];
+        key_value = key[2];
+        where = where" "key_field" = \x24"key_value"\x24 AND";
+      }
+      where = substr(where, 1, length(where) - 4);
+
+      if (operation == "UPDATE") {
+        if (datetime_format == "DATETIME") {
+
+        } else if (datetime_format == "TIMESTAMP") {
+          print "UPDATE "table" set "datetime_field" = "datetime" WHERE "where";";
+        }
+      }
+    }' ${TMP_DIR}/changes.${TABLE}.out > ${OUTPUT_DIR}/changes.${TABLE}.sql
+
+  done < ${TMP_DIR}/tables
+
+  logInfo "Updating control files"
+
+  while read TABLE
+  do
+    if [ -f ${TMP_DIR}/${TABLE}.failed ]
+    then
+      logWarning "Processing of table '${TABLE}' failed. Control file is not updated "
+      continue
+    fi
+
+    logInfo "Updating control file for table '${TABLE}'"
 
     echo ${TIMESTAMP_NOW} > ${CTRL_DIR}/${TABLE}.timestamp_last_execution
     if [ $? -ne 0 ]
     then
       logError "Command 'echo ${TIMESTAMP_NOW} > ${CTRL_DIR}/${TABLE}.timestamp_last_execution' failed"
-      RETURN_CODE=1
     fi
   done < ${TMP_DIR}/tables
 
