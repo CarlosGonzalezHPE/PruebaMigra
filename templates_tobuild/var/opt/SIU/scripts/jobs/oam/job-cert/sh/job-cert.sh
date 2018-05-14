874766ca71f94bc5dd3f1a09d3d0a15a2724fb97
chcremy <chcremy@1b308f40-575c-0510-bb77-a5e8b53bb2f9>
2018-05-14 17:31:15 +0000

    Mejoras
    git-svn-id: https://draco.esm.cpqcorp.net/svn/DEG_ORANGE/branches/delivery@198 1b308f40-575c-0510-bb77-a5e8b53bb2f9

--- templates_tobuild/var/opt/SIU/scripts/jobs/oam/job-cert/sh/job-cert.sh
+++ templates_tobuild/var/opt/SIU/scripts/jobs/oam/job-cert/sh/job-cert.sh
@@ -33,8 +33,26 @@ function process
   do
     logDebug "Processing certificate '${CERTIFICATE}'"
 
+    LABEL="$(getConfigParam ${CERTIFICATE} LABEL)"
+    if [ $? -ne 0 ] || [ -z "${LABEL}" ]
+    then
+      logError "Unable to get mandatory parameter 'LABEL' in section '${CERTIFICATE}'"
+      RETURN_CODE=1
+      continue
+    fi
+    logDebug "LABEL = ${LABEL}"
+
+    ALIAS="$(getConfigParam ${CERTIFICATE} ALIAS)"
+    if [ $? -ne 0 ] || [ -z "${ALIAS}" ]
+    then
+      logError "Unable to get mandatory parameter 'ALIAS' in section '${CERTIFICATE}'"
+      RETURN_CODE=1
+      continue
+    fi
+    logDebug "ALIAS = ${ALIAS}"
+
     FILEPATH="$(getConfigParam ${CERTIFICATE} FILEPATH)"
-    if [ $? -ne 0 ]
+    if [ $? -ne 0 ] || [ -z "${FILEPATH}" ]
     then
       logError "Unable to get mandatory parameter 'FILEPATH' in section '${CERTIFICATE}'"
       RETURN_CODE=1
@@ -43,7 +61,7 @@ function process
     logDebug "FILEPATH = ${FILEPATH}"
 
     PASSPHRASE="$(getConfigParam ${CERTIFICATE} PASSPHRASE)"
-    if [ $? -ne 0 ]
+    if [ $? -ne 0 ] || [ -z "${PASSPHRASE}" ]
     then
       logError "Unable to get mandatory parameter 'PASSPHRASE' in section '${CERTIFICATE}'"
       RETURN_CODE=1
@@ -51,27 +69,24 @@ function process
     fi
     logDebug "PASSPHRASE = ${PASSPHRASE}"
 
-     > ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "#!/bin/bash"
-    >> ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "/usr/bin/expect << EOD"
-    >> ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "spawn keytool -v -list -keystore ${FILEPATH}"
-    >> ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "set timeout 4"
-    >> ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "expect \"Enter keystore password:\""
-    >> ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "send \"${PASSPHRASE}\r\""
-    >> ${TMP_DIR}/keytool-${CERTIFICATE}.sh echo "EOD"
-
-    chmod 744 ${TMP_DIR}/keytool-${CERTIFICATE}.sh
-
-    > ${TMP_DIR}/keytool-${CERTIFICATE}.out_err 2>&1 ${TMP_DIR}/keytool-${CERTIFICATE}.sh
+    > ${TMP_DIR}/keytool-${CERTIFICATE}.out_err 2>&1 keytool -v -list -keystore ${FILEPATH} -alias ${ALIAS} -storepass ${PASSPHRASE}
 
     CURRENT_TIMESTAMP=$(date +"%s")
     EXPIRATION_TIMESTAMP=$(date +"%s" -d "$(cat ${TMP_DIR}/keytool-${CERTIFICATE}.out_err | awk '/Valid from:/ { match($0, /.*until: ([: a-zA-Z0-9]+)/, v); print v[1]; }')")
-
     REMAINING_DAYS=$(echo "(${EXPIRATION_TIMESTAMP}-${CURRENT_TIMESTAMP})/24/3600" | bc)
+
+    logDebug "CURRENT_TIMESTAMP = ${CURRENT_TIMESTAMP}"
+    logDebug "EXPIRATION_TIMESTAMP = ${EXPIRATION_TIMESTAMP}"
+    logDebug "REMAINING_DAYS = ${REMAINING_DAYS}"
+
+    logInfo "Remaining days before expiration of certificate '${LABEL}': ${REMAINING_DAYS}"
 
     while read ALARM_LIMIT
     do
+      logDebug "Checking Alarm Limit '${ALARM_LIMIT}'"
+
       THRESHOLD_DAYS="$(getConfigParam ${ALARM_LIMIT} THRESHOLD_DAYS)"
-      if [ $? -lt 0 ] || [ -z ${THRESHOLD_DAYS} ]
+      if [ $? -lt 0 ] || [ -z "${THRESHOLD_DAYS}" ]
       then
         logError "Unable to get mandatory parameter 'THRESHOLD_DAYS' in section '${ALARM_LIMIT}'"
         return 1
@@ -81,9 +96,10 @@ function process
       if [ ${REMAINING_DAYS} -le ${THRESHOLD_DAYS} ]
       then
         logWarning "Alarm condition detected for certificate '${CERTIFICATE}'"
+        > ${TMP_DIR}/alarm_condition
 
         ALARM_ID="$(getConfigParam ${ALARM_LIMIT} ALARM_ID)"
-        if [ $? -lt 0 ] || [ -z ${THRESHOLD_DAYS} ]
+        if [ $? -lt 0 ] || [ -z "${THRESHOLD_DAYS}" ]
         then
           logError "Unable to get mandatory parameter 'ALARM_ID' in section '${ALARM_LIMIT}'"
           return 1
@@ -91,7 +107,7 @@ function process
         logDebug "ALARM_ID = ${ALARM_ID}"
 
         ALARM_TEXT="$(getConfigParam ${ALARM_LIMIT} ALARM_TEXT)"
-        if [ $? -lt 0 ] || [ -z ${THRESHOLD_DAYS} ]
+        if [ $? -lt 0 ] || [ -z "${THRESHOLD_DAYS}" ]
         then
           logError "Unable to get mandatory parameter 'ALARM_TEXT' in section '${ALARM_LIMIT}'"
           return 1
@@ -105,6 +121,11 @@ function process
       fi
     done < ${TMP_DIR}/alarm_limits
   done < ${TMP_DIR}/certificates
+
+  if [ ! -f ${TMP_DIR}/alarm_condition ]
+  then
+    logInfo "No alarm condition detected"
+  fi
 
   return ${RETURN_CODE}
 }
